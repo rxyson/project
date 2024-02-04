819ee5387e84f2cb221dbdadd7038777702a57db
rxyson <157884558+rxyson@users.noreply.github.com>
2024-02-04 19:39:19 +0300

    Update final_win.py

--- final_win.py
+++ final_win.py
@@ -1,1 +1,30 @@
-print('Напишите ваш возраст')
+from PyQt5.QtCore import Qt, QTimer, QTime, QLocale
+from PyQt5.QtGui import QFont, QDoubleValidator, QIntValidator
+from PyQt5.QtWidgets import (
+    QApplication, QWidget,
+    QHBoxLayout, QVBoxLayout,
+    QGroupBox, QRadioButton,
+    QPushButton, QLabel, QListWidget, QLineEdit)
+
+from my_app import *
+from second_win import *
+from instr import *
+
+class FinalWin(QWidget):
+    def __init__(self):
+       super().__init__()
+       self.set_appear()
+       self.initUI()
+       self.show()
+    def set_appear(self):
+       self.setWindowTitle(txt_title)
+       self.resize(win_width, win_height)
+       self.move(win_x, win_y)
+    def initUI(self):
+       self.workh_text = QLabel(txt_workheart)
+       self.index_text = QLabel(txt_index)
+
+       self.layout_line = QVBoxLayout()
+       self.layout_line.addWidget(self.index_text, alignment= Qt.AlignCenter)
+       self.layout_line.addWidget(self.workh_text, alignment= Qt.AlignCenter)
+       self.setLayout(self.layout_line)
